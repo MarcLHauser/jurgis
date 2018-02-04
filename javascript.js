ea6d400d63e77ed567197a815a1874140655a288
Marc H <mhauser@ocean7.com>
2018-02-04 16:41:39 -0500

    initial commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+var questions = ["You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'", "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?", "The man makes you pay enormous fees to get out. Once you get out of the hotel, you remember that your friend got rich in Chicago. Do you want to go to Chicago?"];
+
+var player = {
+    name: "Jurgis",
+    alive: true,
+    place: "Lithuania",
+    causeDeath: "a cough",
+    questionNumber: questions[0]
+};
+
+var ask = function(){
+    
+    if(player.alive){
+       
+        console.log(player.questionNumber);
+        return "";
+    
+    } else {
+    
+        console.log("You have died in " + player.place + " of " + player.causeDeath);
+        return "Game Over.";
+    }
+
+};
+
+var n = 0;  
+
+var yes = function(){
+    n = n + 1;
+    player.questionNumber = questions[n];
+    return ask();
+};
+
+var no = function(){
+    player.alive = false;
+    player.causeDeath = "cold climate";
+    return ask();
+};
+
+ask();
