df016b5c119631ea380dfd6c8e12f328932f94b1
Marc H <mhauser@ocean7.com>
2018-02-04 18:00:48 -0500

    map wrong answser

--- javascript.js
+++ javascript.js
@@ -4,18 +4,25 @@ var map = {
         place: "Lithuania",
         question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'",
         yes: "New York",
+        no: "Freeze"
     },
     
     "New York" : {
         place: "New York",
         question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?",
         yes: "Hotel",
+        no: "Freeze"
     },
     
     "Hotel" : {
         place: "Hotel",
         question: "The man makes you pay enormous fees to get out. Once you get out of the hotel, you remember that your friend got rich in Chicago. Do you want to go to Chicago?",
         yes: "Lithuania",
+        no: "Freeze"
+    },
+
+    "Freeze" : {
+        causeDeath : "the cold climate"
     }
 };
 
@@ -30,13 +37,12 @@ var player = {
 var ask = function(){
     
     if(player.alive){
-       
+        player.place = player.location.place;
         console.log(player.location.question);
         return "";
     
     } else {
-    
-        console.log("You have died in " + player.place + " of " + player.causeDeath);
+        console.log("You have died in " + player.place + " of " + player.location.causeDeath);
         return "Game Over.";
     }
 
@@ -45,12 +51,12 @@ var ask = function(){
 
 
 var yes = function(){
-
     player.location = map[player.location.yes];
     return ask();
 };
 
 var no = function(){
+    player.location = map[player.location.no];
     player.alive = false;
     player.causeDeath = "cold climate";
     return ask();
